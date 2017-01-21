ed7447943bb759de8b03aa7bb34d5868eba1901d
DIMA_N <dim9044@yandex.ru>
2017-01-22 00:15:55 +0300

    Создаем БД для категорий

--- app/src/main/java/ru/n_develop/n_calcs/Activity/MainActivity.java
+++ app/src/main/java/ru/n_develop/n_calcs/Activity/MainActivity.java
@@ -1,19 +1,32 @@
-package ru.n_develop.n_calcs;
+package ru.n_develop.n_calcs.Activity;
 
 import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 
+import ru.n_develop.n_calcs.Helper.DBHelper;
+import ru.n_develop.n_calcs.R;
+
 public class MainActivity extends AppCompatActivity
 {
+
+
+    DBHelper dbHelper;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        dbHelper = new DBHelper(this);
+
+        SQLiteDatabase database = dbHelper.getWritableDatabase();
+
     }
 
     // запуск окна с деревянными окнами
---
+++ app/src/main/java/ru/n_develop/n_calcs/Helper/DBHelper.java
@@ -0,0 +1,68 @@
+package ru.n_develop.n_calcs.Helper;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by dim90 on 18.01.2017.
+ */
+
+public class DBHelper extends SQLiteOpenHelper
+{
+
+    public static final int DATABASE_VERSION = 2;
+    public static final String DATABASE_NAME = "Calc";
+    public static final String TABLE_CATEGORY = "category";
+
+
+    // _ нужен для работы с курсорами, это особенность android
+    public static final String KEY_ID_CATEGIRY = "_id_category";
+    public static final String KEY_NAME_CATEGORY = "name_category";
+    public static final String KEY_IMAGE = "image";
+    public static final String KEY_DATE_CREATED = "date_created";
+    public static final String KEY_DATE_UPDATED = "date_updated";
+
+
+    public DBHelper(Context context)
+    {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db)
+    {
+        ContentValues contentValues = new ContentValues();
+
+        db.execSQL("CREATE TABLE " + TABLE_CATEGORY +
+                " ( " + KEY_ID_CATEGIRY + " integer primary key," +
+                KEY_NAME_CATEGORY + " text, " +
+                KEY_IMAGE + " text, " +
+                KEY_DATE_CREATED + " text, " +
+                KEY_DATE_UPDATED + " text) "
+        );
+
+        contentValues.put(KEY_NAME_CATEGORY, "Математика");
+        contentValues.put(KEY_IMAGE, "");
+        contentValues.put(KEY_DATE_CREATED, "");
+        contentValues.put(KEY_DATE_UPDATED, "");
+
+
+        contentValues.put(KEY_NAME_CATEGORY, "Физика");
+        contentValues.put(KEY_IMAGE, "");
+        contentValues.put(KEY_DATE_CREATED, "");
+        contentValues.put(KEY_DATE_UPDATED, "");
+
+        db.insert(TABLE_CATEGORY, null, contentValues);
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion)
+    {
+        db.execSQL("drop table if exists " + TABLE_CATEGORY);
+
+        onCreate(db);
+    }
+}
